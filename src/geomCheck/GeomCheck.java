1c4c69e89c783aefd259673848e3f66c9f197d6a
NI240SX <creeper57frm@gmail.com>
2024-02-18 22:57:34 +0100

    GeomCheck v1.0

--- src/geomCheck/GeomCheck.java
+++ src/geomCheck/GeomCheck.java
@@ -5,14 +5,12 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 
 import binstuff.Hash;
